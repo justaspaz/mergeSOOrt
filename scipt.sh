5cdceac83b034157cdcd475ac5114dce4d9941bb
justaspaz <55506752+justaspaz@users.noreply.github.com>
2024-12-23 20:59:32 +0200

    Update scipt.sh

--- scipt.sh
+++ scipt.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #SBATCH -p main       # Queue name
-#SBATCH -n128         # Number of processes
-module load openmpi
+#SBATCH -n64         # Number of processes
 mpicc -o mergeSort mergeSort.c
 mpirun -np 128 mergeSort
