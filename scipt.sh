2ea25de3b9354d4073b3cf993af0e4563bea4333
justaspaz <55506752+justaspaz@users.noreply.github.com>
2024-12-23 20:26:15 +0200

    Update scipt.sh

--- scipt.sh
+++ scipt.sh
@@ -2,8 +2,5 @@
 #SBATCH -p main       # Queue name
 #SBATCH -n256         # Number of processes
 module load openmpi
-# Compile the program
 mpicc -o mergeSort mergeSort.c
-
-# Run the program with increasing number of processes
 mpirun -np 256 mergeSort
