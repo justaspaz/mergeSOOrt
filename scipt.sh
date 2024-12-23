75964be47e81f9c80f81311c9c848d10f31d5ad7
justaspaz <55506752+justaspaz@users.noreply.github.com>
2024-12-23 20:09:58 +0200

    Update scipt.sh

--- scipt.sh
+++ scipt.sh
@@ -6,12 +6,4 @@
 mpicc -o mergeSort mergeSort.c
 
 # Run the program with increasing number of processes
-mpirun -np 1 mergeSort
-mpirun -np 2 mergeSort
-mpirun -np 4 mergeSort
-mpirun -np 8 mergeSort
-mpirun -np 16 mergeSort
-mpirun -np 32 mergeSort
-mpirun -np 64 mergeSort
-mpirun -np 128 mergeSort
 mpirun -np 256 mergeSort
